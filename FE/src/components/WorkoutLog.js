927ffc576072b54ecf79b707c3432c826eecd1c9
unknown <epeng1994@gmail.com>
2022-11-17 13:59:47 -0500

    created function to find Sunday of current week

--- FE/src/components/WorkoutLog.js
+++ FE/src/components/WorkoutLog.js
@@ -5,15 +5,15 @@ import axios from 'axios';
 const filteredData= [
     {
         "user_id": "1",
-        "date": "2022-11-10",
+        "date": "2022-11-14",
     },
     {
         "user_id": "2",
-        "date": "2022-11-12",
+        "date": "2022-11-16",
     },
     {
         "user_id": "1",
-        "date": "2022-11-14"
+        "date": "2022-11-16"
     },
 ]
 
@@ -33,7 +33,24 @@ function WeeklyBar(props){
                 console.log(err)
             })
     }
-    //on submit will sent a post to backend stating workout on today
+
+    const currentWeek = e =>{
+        const day = new Date();
+        let weekday = day.toDateString().split(' ')[0];
+        let startDate = new Date(day.getTime());
+
+        for(let i =1;i<7;i++){
+            if(weekday === 'Sun')break;
+            startDate.setDate(day.getDate()-i);
+            weekday = startDate.toDateString().split(' ')[0];
+        };
+        //console.log(startDate)
+        return startDate;
+    };
+
+
+
+
     return(
         <>  
             <Tracker dates = {dates}/>
@@ -43,6 +60,7 @@ function WeeklyBar(props){
             </form>
             <div>{errorMsg}</div>
             {message ? <div>{message}</div>: null}
+            <button onClick = {()=>currentWeek()}>Day</button>
         </>
     )
 }
