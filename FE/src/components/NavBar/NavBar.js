148a1263f4565e2725f623d85b397688f4e2509c
unknown <epeng1994@gmail.com>
2022-11-22 22:06:58 -0500

    removed unneeded css and added css to playlist component

--- FE/src/components/NavBar/NavBar.js
+++ FE/src/components/NavBar/NavBar.js
@@ -1,23 +1,19 @@
 import { Link } from "react-router-dom";
-import react from 'react'
 import './NavBar.css'
 
 
 function NavBar(props){
-
-    
+   
 
 
     return(
-        <div className = 'container'>
-            <nav>
-                <Link to='/' className = 'navBlock'>Home</Link>
-                <Link to='/workoutlog' className = 'navBlock'>Workout Log</Link>
-                <Link to='/spotify' className = 'navBlock'>Spotify</Link>
-                <Link to='/calendar' className = 'navBlock'>Calendar</Link>
-                <Link to='/report' className = 'navBlock'>Report</Link>
-            </nav>
-        </div>
+        <nav>
+            <Link to='/' className = 'navBlock'>Home</Link>
+            <Link to='/workoutlog' className = 'navBlock'>Workout Log</Link>
+            <Link to='/spotify' className = 'navBlock'>Spotify</Link>
+            <Link to='/calendar' className = 'navBlock'>Calendar</Link>
+            <Link to='/report' className = 'navBlock'>Report</Link>
+        </nav>
     )
 }
 
