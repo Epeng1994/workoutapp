d535f23e02f4079d547437ec9fdf5d6367441aae
unknown <epeng1994@gmail.com>
2022-12-06 23:24:40 -0500

    initiated timer componenent

---
+++ FE/src/components/Timer/Timer.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+
+function Timer(props){
+    return(
+        <>
+
+        </>
+    );
+};
+
+export default Timer;
+/*
+    display HH:MM:SS
+    Start/Stop
+    Reset
+    Lap
+*/
