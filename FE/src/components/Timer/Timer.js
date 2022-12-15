3bd7c3ec8c058b2e041fb252a7379172cfbb833b
unknown <epeng1994@gmail.com>
2022-12-15 01:38:47 -0500

    completed timer component

--- FE/src/components/Timer/Timer.js
+++ FE/src/components/Timer/Timer.js
@@ -1,46 +1,60 @@
 import { useState, useEffect } from 'react';
-
-
-const dummyLaps = [
-    100,
-    6000,
-    36000
-]
-
+import './Timer.css';
 
 function Timer(props){
     const [time, setTime] = useState(0);
-    const [lap, setLaps] = useState(dummyLaps)
+    const [lap, setLaps] = useState([])
     const [mode,setMode] = useState('Start')
+    const [timer,setTimer] = useState(false)
 
     useEffect(()=>{
-
-    },[mode])
-
+        if(timer){
+            const interval = setInterval(() => {
+                setTime(time=>time+1);
+              }, 10);
+              return () => clearInterval(interval);
+        }
+    },[timer])
 
     const modeChange = e =>{
         const {value}=e.target
-        setMode(value)
+        if(value==='Start'||value==='Resume'){
+            setMode('Stop')
+            setTimer(true)
+        }else if(value==='Stop'){
+            setMode('Resume')
+            setTimer(false)
+        }
     }
 
     const resetTimer = e =>{
         setMode('Start')
+        setTime(0)
+        setLaps([])
+        setTimer(false)
+    }
+
+    const addLap = e =>{
+        setLaps([time,...lap,])
     }
 
     return(
         <>
-            {time}
-            <button value = {mode === 'Start' ? 'Stop' : mode === 'Stop' ? 'Resume' : 'Stop'} onClick = {e=>modeChange(e)}>{mode}</button>
-            <button onClick = {resetTimer}>Lap/Reset</button>
-
+            <div>{(Math.floor(time/360000)%24<10?'0':'') + Math.floor(time/360000)%24}:{(Math.floor(time/6000)%60<10?'0':'') + Math.floor(time/6000)%60}:{(Math.floor(time/100)%60 < 10? `0`:'')+Math.floor(time/100)%60}:{(time%100 < 10? `0`:'')+time%100}</div>
+            <button value = {mode} onClick = {e=>modeChange(e)}>{mode}</button>
+            {
+                time === 0 ? '' : timer===true ? <button onClick = {addLap}>Lap</button> : <button onClick = {resetTimer}>Reset</button>
+            }
+            
+            <div className = 'lapContainer'>
+                {lap.map((time,iterate)=>{
+                    return(
+                        <div>{lap.length-iterate} {(Math.floor(time/360000)%24<10?'0':'') + Math.floor(time/360000)%24}:{(Math.floor(time/6000)%60<10?'0':'') + Math.floor(time/6000)%60}:{(Math.floor(time/100)%60 < 10? `0`:'')+Math.floor(time/100)%60}:{(time%100 < 10? `0`:'')+time%100}</div>
+                    )
+                })}
+            </div>
         </>
     );
 };
 
 export default Timer;
-/*
-    display HH:MM:SS
-    Start/Stop
-    Reset
-    Lap
-*/
