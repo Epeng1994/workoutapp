78d43ea9650eb55dad3b2e6191a3bf4a89c8944c
unknown <epeng1994@gmail.com>
2023-01-13 15:38:59 -0500

    added spread operator for redux state

--- FE/src/reducers/index.js
+++ FE/src/reducers/index.js
@@ -1,16 +1,19 @@
-import { FETCH_SPOTIFY_CATEGORIES, SEARCH_SPOTIFY_CATEGORY } from '../actions/index.js';
+import { FETCH_SPOTIFY_CATEGORIES, SEARCH_SPOTIFY_CATEGORY, CLEAR_SPOTIFY_PLAYLIST } from '../actions/index.js';
 
 
-
-export default function(state=[1,2,3],action){
+export default function(state={},action){
     switch(action.type){
         case FETCH_SPOTIFY_CATEGORIES:
             return {
-                categoryData:action.payload
+                ...state,categoryData:action.payload
             }
         case SEARCH_SPOTIFY_CATEGORY:
             return {
-                currentPlaylist: action.payload
+                ...state,currentPlaylist: action.payload
+            }
+        case CLEAR_SPOTIFY_PLAYLIST:
+            return {
+                ...state,categoryData: action.payload
             }
         default:
             return state;
