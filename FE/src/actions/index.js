5f0d1578b5ff6b269139346e6eb9f4bed6dedfbb
unknown <epeng1994@gmail.com>
2023-01-13 15:40:40 -0500

    connected redux state to playlist component

--- FE/src/actions/index.js
+++ FE/src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_SPOTIFY_CATEGORIES = 'FETCH_SPOTIFY_CATEGORIES';
 export const SEARCH_SPOTIFY_CATEGORY = 'SEARCH_SPOTIFY_CATEGORY';
+export const CLEAR_SPOTIFY_PLAYLIST = 'CLEAR_SPOTIFY_PLAYLIST';
 
 export function fetchCategoryData(token){
     return async function(dispatch){
@@ -28,8 +29,21 @@ function setCategoryData(data){
     return{
         type:FETCH_SPOTIFY_CATEGORIES,
         payload:data
-    }
-}
+    };
+};
+
+export function clearPlaylistData(){
+    return async function(dispatch){
+        return dispatch(clearPlaylist())
+    };
+};
+
+function clearPlaylist(){
+    return{
+        type:CLEAR_SPOTIFY_PLAYLIST,
+        payload:{}
+    };
+};
 
 export function searchByCategory(category_id,token){
     return async function(dispatch){
@@ -45,12 +59,12 @@ export function searchByCategory(category_id,token){
         .catch(error=>{
             console.log(error.response.data.error.message)
         })
-    }
-}
+    };
+};
 
 function setCurrentCategoryData(data){
     return{
         type:SEARCH_SPOTIFY_CATEGORY,
         payload:data
-    }
-}
+    };
+};
--- FE/src/components/SpotifyPage/Playlist.js
+++ FE/src/components/SpotifyPage/Playlist.js
@@ -1,11 +1,9 @@
 import {useState} from 'react';
 import './Spotify.css'
-
+import { connect } from 'react-redux'
 
 function Playlist(props){
-    const { data } = props;
     const [visible,setVisible] = useState(false)
-
     const Redirect = e =>{
         window.location.replace(e)
     }
@@ -16,14 +14,20 @@ function Playlist(props){
 
     return(
         <div className = "playlist">
-            <img src = {data.images[0].url}/>
+            <img src = {props.currentPlaylist.images[0].url}/>
             <div onClick = {e=>Toggle(e.target)} className = {`playlistInfo fade-${visible?'in':'out'}`}>
-                <h2>{data.name}</h2>
-                <p className = "playlistDescription">{data.description}</p>
+                <h2>{props.currentPlaylist.name}</h2>
+                <p className = "playlistDescription">{props.currentPlaylist.description}</p>
             </div>
-            <button className ="" onClick = {()=>Redirect(data.external_urls.spotify)}>Listen Now on Spotify</button>
+            <button className ="" onClick = {()=>Redirect(props.currentPlaylist.external_urls.spotify)}>Listen Now on Spotify</button>
         </div>
     )
 }   
 
-export default Playlist;
+const mapStateToProps = state =>{
+    return {
+        currentPlaylist: state.currentPlaylist
+    }
+}
+
+export default connect(mapStateToProps,{})(Playlist);
